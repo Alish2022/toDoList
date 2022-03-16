d3492edae194fd35f21a7b2ad0c3a9797c0b42c3
Alish2022 <98400305+Alish2022@users.noreply.github.com>
2022-03-16 16:55:37 +0300

    160322

--- src/AddTask.tsx
+++ src/AddTask.tsx
@@ -1,38 +1,14 @@
-import React, {ChangeEvent, KeyboardEvent, useState} from 'react';
+import React from 'react';
+import AddItem from "./AddItem";
 
 type addTaskType = {
     addTask: (newTask: string) => void
 }
 
 const AddTask: React.FC<addTaskType> = (props) => {
-
-    const [newTask, setNewTask] = useState<string>('')
-    const [error, setError] = useState<string | null>(null)
-
-    const onChangeInputHandler = (event: ChangeEvent<HTMLInputElement>) => {
-        setNewTask(event.currentTarget.value)
-    }
-    const onClickButtonHandler = () => {
-        if (newTask.trim() !== '') {
-            props.addTask(newTask.trim())
-            setNewTask('')
-            setError(null)
-        } else {
-            setError("Field is required")
-        }
-    }
-
-    const onKeyPressHandler = (e: KeyboardEvent<HTMLInputElement>) => {
-        if (e.key==='Enter'){
-            onClickButtonHandler()
-        }
-    }
-
     return (
         <div>
-            <input className={error?"errorInput":''} onKeyPress={onKeyPressHandler} onChange={onChangeInputHandler} value={newTask} type={"text"}/>
-            <button onClick={onClickButtonHandler}>+</button>
-            {error && <div className={"errorMessage"}>{error}</div>}
+            <AddItem addItem={props.addTask}/>
         </div>
     );
 };
--- src/EditableSpan.tsx
+++ src/EditableSpan.tsx
@@ -1,36 +1,51 @@
-import React, {ChangeEvent, useState} from 'react';
+import React, {ChangeEvent, KeyboardEvent, useState} from 'react';
 
-type propsType={
-    title:string
-    edit:(newTitle:string)=>void
+type propsType = {
+    title: string
+    edit: (newTitle: string) => void
 }
 
-const EditableSpan:React.FC<propsType> = (props) => {
+const EditableSpan: React.FC<propsType> = (props) => {
 
-    const [editMode,setEditMode]=useState<boolean>(false)
-    const [title,setTitle]=useState<string>(props.title)
+    const [editMode, setEditMode] = useState<boolean>(false)
+    const [title, setTitle] = useState<string>(props.title)
+    const [error, setError] = useState<boolean>(false)
 
-    const onChangeHandler=(e:ChangeEvent<HTMLInputElement>)=>{
+    const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
         setTitle(e.currentTarget.value)
         console.log(title)
     }
 
-    const onClickHandler=()=>{
+    const onClickHandler = () => {
         setEditMode(true)
     }
 
-    const onBlurHandler=()=>{
-        setEditMode(false)
-        props.edit(title)
+    const onBlurHandler = () => {
+        if (title.trim() === "") {
+            setError(true)
+        } else {
+            setEditMode(false)
+            props.edit(title)
+        }
+    }
+
+    const onKeyPressHandler = (e: KeyboardEvent<HTMLInputElement>) => {
+        if (e.key === 'Enter') {
+            onBlurHandler()
+        }
     }
 
     return (
         <>
-            {editMode?
-                <input value={title} onBlur={onBlurHandler} onChange={onChangeHandler}  type={"text"} autoFocus/>
-                :<span onClick={onClickHandler}>{props.title}</span>}
+            {editMode ?
+                <input value={title} onKeyPress={onKeyPressHandler} onBlur={onBlurHandler} onChange={onChangeHandler}
+                       type={"text"} autoFocus/>
+                {error&& <div className={"errorMessage"}>{error}</div>{
+                : <span onClick={onClickHandler}>{props.title}</span>}
         </>
     );
 };
 
 export default EditableSpan;
+
+
